190464e59c46f3225d13cc9501bf0c5f21d9d226
DMancia03 <diego.mancia.03@gmail.com>
2024-07-20 22:23:16 -0600

    feature: Add confirmational modal for clear cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,6 +5,18 @@ import Swal from "sweetalert2"
 import withReactContent from "sweetalert2-react-content"
 
 export const Cart = ({allProducts, total, onCleanCart, active, onDeleteProduct}) => {
+    const confirm_onCleanCart = () => {
+        withReactContent(Swal).fire({
+            html: '<p>¿Estás seguro de vaciar tu <b>carrito de compras</b>?</p>',
+            //text: '',
+            icon: 'warning',
+            showCancelButton: true
+        }).then((result) => {
+            if(result.isConfirmed){
+                onCleanCart()
+            }
+        });
+    }
     return (
         <>
             <div
@@ -21,7 +33,7 @@ export const Cart = ({allProducts, total, onCleanCart, active, onDeleteProduct})
                                 <h3>Total:</h3>
                                 <span className="total-pagar">${total}</span>
                             </div>
-                            <button className="btn-clear-all" onClick={onCleanCart}>Variar Carrito</button>
+                            <button className="btn-clear-all" onClick={confirm_onCleanCart}>Vaciar Carrito</button>
                         </>
                     ) : (
                         <p className='cart-empty'>El carrito está vacío</p>
